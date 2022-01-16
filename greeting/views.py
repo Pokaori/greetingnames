5ccbe172c6504ebb4efcf81e789c338e26effe96
Pokaori <sempolina15@gmail.com>
2022-01-16 23:42:23 +0200

    change views

--- greeting/views.py
+++ greeting/views.py
@@ -12,18 +12,14 @@ class Greeting(View):
         return render(request, 'home.html', {})
 
     def post(self, request):
-        data=json.loads(request.body)
+        data = json.loads(request.body)
         try:
             name = Names.objects.create(full_name=data["full_name"])
         except IntegrityError as e:
-            return JsonResponse({"error":str(e).split('\n')[0]},status=409)
+            return JsonResponse({"error": str(e).split('\n')[0]}, status=409)
         except Exception as e:
             return JsonResponse({"error": str(e).split('\n')[0]}, status=400)
-        return JsonResponse({"full_name":name.full_name},status=200)
-
-
-def home_view(request):
-    return render(request, 'home.html', {})
+        return JsonResponse({"full_name": name.full_name}, status=200)
 
 
 class NamesListView(ListView):
